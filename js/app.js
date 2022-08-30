0cca73b9dbe859aec907aca4ec691c8fa141e239
sufian <sufian4se@gmail.com>
2022-08-30 19:20:36 +0600

    implement show all button to display all data

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayPhones(data.data);
+  displayPhones(data.data, dataLimit);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, dataLimit) => {
   const phonesContainer = document.getElementById("phones-container");
   phonesContainer.textContent = "";
-  // display 20 phones only
-  phones = phones.slice(0, 10);
+  // display 10 phones only
+  const showAll = document.getElementById("show-all");
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 10);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
 
   // display no phones
-  const noPhone = document.getElementById("no-phone-message");
+  const noPhone = document.getElementById("no-found-message");
   if (phones.length === 0) {
     noPhone.classList.remove("d-none");
   } else {
@@ -35,16 +41,20 @@ const displayPhones = (phones) => {
     phonesContainer.appendChild(phoneDiv);
   });
   // stop spinner or loader
-  toggleSpinner(false)
+  toggleSpinner(false);
+};
+
+const processSearch = (dataLimit) => {
+  toggleSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadPhones(searchText, dataLimit);
 };
 
 // handle search button click
 document.getElementById("btn-search").addEventListener("click", function () {
   // start loader
-  toggleSpinner(true);
-  const searchField = document.getElementById("search-field");
-  const searchText = searchField.value;
-  loadPhones(searchText);
+  processSearch(10);
 });
 
 const toggleSpinner = (isLoading) => {
@@ -55,5 +65,10 @@ const toggleSpinner = (isLoading) => {
     loaderSection.classList.add("d-none");
   }
 };
+
+// not the best way to load show all
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  processSearch();
+});
 
 // loadPhones();
