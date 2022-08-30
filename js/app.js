4ee1ac63c6703b39423889dd26b0421328b0722c
sufian <sufian4se@gmail.com>
2022-08-30 07:06:10 +0600

    load data using phone hunter api and display phones and implement sear ch functionality

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const loadPhones = async (searchText) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhones(data.data);
+};
+
+const displayPhones = (phones) => {
+  // console.log(phones)
+  const phonesContainer = document.getElementById("phones-container");
+  phonesContainer.textContent = '';
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top p-4" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+    </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+  });
+};
+
+document.getElementById('btn-search').addEventListener('click', function() {
+    // console.log('search btn clicked')
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+})
+
+loadPhones();
